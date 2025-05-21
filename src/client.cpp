7d129466f63850fb345346b7ff06f3270b62455c
Magform <nicolas25.02.2003.nf@gmail.com>
2025-05-21 09:36:07 +0200

    Idea for implementation

---
+++ src/client.cpp
@@ -0,0 +1,12 @@
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+
+    //Wait for the ROS2 service call [file service.srv]
+    
+
+    //If recived and the ID match with the self id start the file reader and
+    //    read data from the file and send them to a ros2 topic
+    //[Il formato dei file e' un formato msg custom]
+
+    return 0;
+}
